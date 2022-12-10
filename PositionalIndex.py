370a11171f279238778b1d24b2d3336490b2eef6
mohammedAlySawalhy <muh.aly@outlook.com>
2022-12-10 22:49:14 +0200

    musamir pos_index

--- PositionalIndex.py
+++ PositionalIndex.py
@@ -1,9 +1,27 @@
-
 import os
 import json
 
+example = [
+    {
+        "term": "TheTerm",
+        "docs_count": 2,
+        "docs": {
+            "1.txt": [1, 100, 340],
+            "6.txt": [2, 51]
+        }
+    }
+]
 
-class positionalIndex:
+# -> we can modify the tokanization process to calculate positions as well
+# -> less effecient method is to calculate the position in separate step using KMP for example
+#
+# Next step: merging positions
+# Q1: intersect the query with the position index, what does that mean?
+# Q2: what does poistion merging mean?
+#
+
+
+class PositionalIndex:
     def __init__(self, inv_index_path, output_dir, file_name):
         self.output_dir = output_dir
         self.inv_index_path = inv_index_path
@@ -19,27 +37,30 @@ class positionalIndex:
         return positions
 
     def create_positional_index(self):
-        positionalIndex = {}
+        pos_index = {}
         with open(self.inv_index_path, 'r') as f:
-            for line in f:
-                line = json.loads(line)
-                terms = list(line.keys())
-                for term in terms:
-                    docs = line[term]
-                    for doc in docs:
-                        position = self.calculate_positions(term, doc)
-                        if term not in positionalIndex:
-                            positionalIndex[term].append(
-                                [len(docs), *position])
-        return positionalIndex
+            inv_index = json.loads(f.read())
+            for term in inv_index.keys():
+                docs = inv_index[term]
+                pos_index[term] = {
+                    "term": term,
+                    "count": 0,
+                    "docs": {}
+                }
+
+                for doc in docs:
+                    positions = self.calculate_positions(term, doc)
+                    pos_index[term]["count"] += len(positions)
+                    pos_index[term]["docs"][doc] = positions
+
+        return pos_index
 
     def save(self):
         with open(self.output_dir + '/' + self.file_name, 'w') as f:
             f.write(json.dumps(self.create_positional_index()))
 
 
-i = positionalIndex('indexing/invertedindex.txt',
+i = PositionalIndex('indexing/invertedindex.txt',
                     'indexing/', 'positional_index.txt')
 
-i.create_positional_index()
 i.save()

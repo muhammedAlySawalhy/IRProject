819f9b3594b51c5c53bae36a5c0e54a79ebd978b
محمد الصوالحي <ms.2052001@gmail.com>
2022-12-11 02:46:23 +0200

    Implement time-complexity-catastrophe positional indexing

--- PositionalIndex.py
+++ PositionalIndex.py
@@ -1,3 +1,4 @@
+import re
 import os
 import json
 
@@ -26,39 +27,40 @@ class PositionalIndex:
         self.inv_index_path = inv_index_path
         self.out_file = out_file
 
-    def calculate_positions(self, term, doc_id):
-        positions = []
-        with open('docs/' + doc_id, 'r') as f:
-            for line in f:
-                line = line.split()
-                if term not in line: continue
-                positions.append([line.index(term), doc_id])
+    pos_index = {}
 
-        return positions
+    def ensure_term(self, term, doc_file):
+        if term not in self.pos_index:
+            self.pos_index[term] = {
+                "term": term,
+                "count": 0,
+                "docs": {}
+            }
+
+        if doc_file not in self.pos_index[term]["docs"]:
+            self.pos_index[term]["docs"][doc_file] = []
+
+    def insert_term(self, term, doc_file, pos):
+        self.ensure_term(term, doc_file)
+        self.pos_index[term]["count"] += 1
+        self.pos_index[term]["docs"][doc_file].append(pos)
+
+    def process_doc(self, doc_file):
+        with open("./docs/" + doc_file) as f:
+            words = re.compile(r"\s+").split(f.read())
+            # TODO: get tocken from the word
+            for i in range(len(words)):
+                self.insert_term(words[i], doc_file, i + 1)
 
     def create_positional_index(self):
-        pos_index = {}
-        with open(self.inv_index_path, 'r') as f:
-            inv_index = json.loads(f.read())
-            for term in inv_index.keys():
-                docs = inv_index[term]
-                pos_index[term] = {
-                    "term": term,
-                    "count": 0,
-                    "docs": {}
-                }
-
-                for doc in docs:
-                    positions = self.calculate_positions(term, doc)
-                    pos_index[term]["count"] += len(positions)
-                    pos_index[term]["docs"][doc] = positions
-
-        return pos_index
+        for doc_file in os.listdir("./docs"):
+            self.process_doc(doc_file)
 
     def save(self):
+        self.create_positional_index()
         os.makedirs(os.path.dirname(self.out_file), exist_ok=True)
         with open(self.out_file, 'w') as f:
-            f.write(json.dumps(self.create_positional_index()))
+            f.write(json.dumps(self.pos_index))
 
 
 if __name__ == "__main__":
